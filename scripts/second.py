c6f0f964f8520accf30d3ced067c4d6a55f8bbc3
__man0j <bb.bindash35@gmail.com>
2017-09-27 13:50:09 +0545

    scripts

--- scripts/second.py
+++ scripts/second.py
@@ -10,8 +10,8 @@ input_file = argv[1]
 output_file = argv[2]
 template_file = argv[3]
 
-table_names = ['Names','Count of Mentions','Avg. of Betweenness Centrality','Avg. of Inter-Cluster Connectivity',
-               'Sum of Social Engagement','Sum of Published Count']
+table_names = ['Names','Count of Mentions','Avg. of Flow','Avg. of Inter-Cluster Connectivity',
+               'Sum of Social Engagement','Sum of Published Count','Source Rank']
 # Populate contents of 'Raw data' tab (copy/paste from input file)
 workbook = xlsxwriter.Workbook(output_file, {'constant_memory':True})
 worksheet = workbook.add_worksheet("Raw data")
@@ -23,9 +23,12 @@ with open(input_file,'rt', encoding='utf-8') as csvfile:
         for c, col in enumerate(row):
             if col.isdigit():
                 col = int(col)
+                worksheet.write_number(r,c,col)
             elif re.match('^-?[0-9]+\.[0-9]+$', col):
                 col = float(col)
-            worksheet.write(r,c,col)
+                worksheet.write_number(r,c,col)
+            else:
+                worksheet.write(r,c,col)
             higest_col = c
             if(xlsxwriter.utility.xl_col_to_name(c)=='S'):
                 result = re.sub(r'\([^)]*\)', '',col)
@@ -36,21 +39,23 @@ people_name.discard('People (Any Mention)')
 def get_formula(header, row, type=None):
     if  type=='abs':
         if (header == 'Count of Mentions'):
-            return '=COUNTIF($\'Raw data\'.S:S, "*" & A%d & "*")'%(row)
-        elif (header == 'Avg. of Betweenness Centrality'):
-            return '=AVERAGEIF($\'Raw data\'.S:S,"*" & A%d & "*",$\'Raw data\'.AU:AU)' %(row)
+            return '=COUNTIF(\'Raw data\'!S:S, "*" & A%d & "*")'%(row)
+        elif (header == 'Avg. of Flow'):
+            return '=AVERAGEIF(\'Raw data\'!S:S,"*" & A%d & "*",\'Raw data\'!AT:AT)' %(row)
         elif (header == 'Avg. of Inter-Cluster Connectivity'):
-            return '=AVERAGEIF($\'Raw data\'.S:S,"*" & A%d & "*",$\'Raw data\'.AX:AX)'%(row)
+            return '=AVERAGEIF(\'Raw data\'!S:S,"*" & A%d & "*",\'Raw data\'!AX:AX)'%(row)
         elif (header == 'Sum of Social Engagement'):
-            return '=SUMIF($\'Raw data\'.S:S,"*" & A%d & "*",$\'Raw data\'.I:I)' %(row)
+            return '=SUMIF(\'Raw data\'!S:S,"*" & A%d & "*",\'Raw data\'!I:I)' %(row)
         elif (header == 'Sum of Published Count'):
-            return '=SUMIF($\'Raw data\'.S:S,"*" & A%d & "*",$\'Raw data\'.E:E)' %(row)
+            return '=SUMIF(\'Raw data\'!S:S,"*" & A%d & "*",\'Raw data\'!E:E)' %(row)
+        elif (header == 'Source Rank'):
+            return '=IFERROR(AVERAGEIF(\'Raw data\'!S:S,"*" & A%d & "*",\'Raw data\'!AL:AL), 0)' %(row)
         else:
-            return None
+            return ''
     elif type =='per':
         if (header == 'Count of Mentions'):
             return 'PERCENTRANK(B:B,B%d)'%(row)
-        elif (header == 'Avg. of Betweenness Centrality'):
+        elif (header == 'Avg. of Flow'):
             return 'PERCENTRANK(C:C,C%d)' %(row)
         elif (header == 'Avg. of Inter-Cluster Connectivity'):
             return 'PERCENTRANK(D:D,D%d)'%(row)
@@ -58,13 +63,17 @@ def get_formula(header, row, type=None):
             return 'PERCENTRANK(E:E,E%d)' %(row)
         elif (header == 'Sum of Published Count'):
             return 'PERCENTRANK(F:F,F%d)' %(row)
+        elif (header == 'Avg of Flow'):
+            return 'PERCENTRANK(C:C,C%d)' %(row)
+        elif (header == 'Source Rank'):
+            return '=1-PERCENTRANK(G:G,G%d)' %(row)
         else:
-            return None
+            return ''
     else:
         if header == 'Total Score':
-            return '=(G%d*$Weights.$B$2) + (H%d*$Weights.$B$3)+(I%d*$Weights.$B$4)+(J%d*$Weights.$B$5)+ (K%d*$Weights.$B$6)' %(row,row,row,row,row)
+            return '=(H%d*Weights?$B$2) + (I%d*Weights!$B$3)+(J%d*Weights!$B$4)+(K%d*Weights!$B$5)+ (L%d*Weights!$B$6)+(M%d*Weights!$B$7)' %(row,row,row,row,row,row)
         elif header == 'Overall Rank':
-            return '=RANK(L%d,L:L)' %(row)
+            return '=RANK(N%d,N:N)' %(row)
 
 
 wb_read = xlrd.open_workbook(template_file)
